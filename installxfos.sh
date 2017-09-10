4d73459714d568c5c3c4be05a5cb3f15ab4e9f55
hendr5k <hendrik.schmitz03.hs@gmail.com>
2017-09-10 19:13:59 +0200

    Update installxfos.sh

--- installxfos.sh
+++ installxfos.sh
@@ -1,3 +1,3 @@
-sudo apt-get install xfce4 && sudo apt-get install docky && test
+sudo apt-get install xfce4 && sudo apt-get install docky && sudo apt-get remove unity* && test
 docky &
 startxfce4 &
